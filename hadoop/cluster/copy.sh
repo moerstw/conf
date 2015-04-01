8a8314a4ffe54febaf3c066f772e9b743aa3652e
moerstw <moerstw@gmail.com>
2015-04-01 15:30:20 +0800

    update

--- hadoop/cluster/copy.sh
+++ hadoop/cluster/copy.sh
@@ -1,6 +1,8 @@
-cp hosts /etc/hosts
-cp interfaces /etc/network/interfaces
-cp hostname /etc/hostname
-sh /etc/init.d/hostname.sh
+# cp hosts /etc/hosts
+sysctl kernel.hostname=hadoops0
+rsync  ./cluster/hosts /etc/hosts
+# cp ./cluster/interfaces /etc/network/interfaces
+# cp hostname /etc/hostname
+# sh /etc/init.d/hostname.sh
 ifdown eth0
 ifup eth0
